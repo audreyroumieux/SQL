3ab1776ccacbca4d84dfba23f02d828de1f5e20f
Audrey ROUMIEUX <audrey.roumieux@azeo.com>
2020-12-02 18:31:41 +0100

    update Merge with a BULCK example

--- Merge.sql
+++ Merge.sql
@@ -14,24 +14,70 @@ MERGE [dbo].[DimTables] AS D --MERGE INTO [dbo].[DimTables]
 	-- on met a jours les données DimTables avec les données de la source
     WHEN MATCHED THEN
         UPDATE SET D.[Description]			 = S.[Description]
-                  ,D.[SerialNumber]			 = UPPER(S.[ManufSerialNumber])
-				  ,D.[Region]				 = COALESCE( S.[Region], S.[Departement])
-				  ,D.[StartupDate]			 = CONVERT(datetime2, S.[StartupDate], 104)
+                  ,D.[column_1]			 	 = UPPER(S.[column_1])
+				  ,D.[column_2]				 = COALESCE( S.[column_2], S.[column_3])
+				  ,D.[StartupDate]			 = CONVERT(datetime2, S.[StartupDate], 121)
 	
 	-- s'il y a des lignes dans source, mais pas dans la table dim
 	-- on les rajoutes		  
     WHEN NOT MATCHED BY TARGET THEN	
 		INSERT ([Description]
 				,[PrimaryKey]
-				,[SerialNumber]
-				,[Region]
+				,[column_1]
+				,[column_2]
 				,[StartupDate])
         VALUES (S.[Description]
         ,S.[PrimaryKey]
-        ,UPPER(S.[ManufSerialNumber])
-		,COALESCE( S.[Region], S.[Departement])
+        ,UPPER(S.[column_1])
+		,COALESCE( S.[column_2], S.[column_3])
 		,CONVERT(datetime2, S.[ImportDate], 121))
 	/*
 	-- on suprime les lignes dans dim, qui ne sont pas dans staging	
 	 WHEN NOT MATCHED BY SOURCE THEN DELETE */ 
 	 ;
+
+
+
+/************************Pour Postgresql utilisison : UPSERT *******************************/
+
+INSERT INTO [dbo].[DimTables] (PrimaryKey, Description)
+VALUES (PrimaryKey_value, Description_new_value)
+ON CONFLICT (PrimaryKey)
+--ON CONFLICT ON CONSTRAINT DimTables_PK
+DO UPDATE SET Description = EXCLUDED.Description_new_value || ';' || [DimTables].Description
+--DO NOTHING;
+
+
+
+/********************************** BULK INSERT **********************************
+-- 1 - Créez une table TEMPORAIRE
+-- 2 - Copiez ou insérez les nouvelles données dans la table temporaire
+-- 3 - LOCK la table cible IN EXCLUSIVE MODE. Cela sélectionne d'autres transactions, mais ne modifie pas la table.
+-- 4 - UPDATE ... à partir des enregistrements existants dans la table temporaire
+-- 5 - INSERT les ligne qui n'existe pas encore dans la table
+-- 6 - COMMIT, en libérant le verrou
+/********************************************************************/
+
+BEGIN;
+
+CREATE TEMPORARY TABLE Source(id integer, column_1 text, column_2 text);
+
+INSERT INTO Source(id, column_1, column_2) VALUES (2, 'John', 'Do'), (3, 'Alan', 'Turing');
+
+LOCK TABLE [dbo].[DimTables] IN EXCLUSIVE MODE;
+
+UPDATE [dbo].[DimTables]
+SET column_1 = Source.column_1,
+column_2 = Source.column_2
+FROM Source
+WHERE Source.id = [DimTables].id;
+
+INSERT INTO [dbo].[DimTables]
+SELECT Source.id, Source.column_1, Source.column_2
+FROM Source
+LEFT OUTER JOIN [dbo].[DimTables] ON ([DimTables].id = Source.id)
+WHERE [DimTables].id IS NULL;
+
+COMMIT;
+
+
